812583f216515784a83b1194ff7ea2f273ff8b80
Pieter Brands <brandspieter@hotmail.com>
2018-11-16 12:08:16 +0100

    swag

---
+++ Project Keyboard/App/Model/Gebruiker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// Summary description for Class1
+/// 
+public class Gebruiker
+{
+    private string gebruikersnaam { get; set; }
+    private string password { get; set; }
+    private string voornaam { get; set; }
+    private string achternaam { get; set; }
+    private int leeftijd { get; set; }
+
+    public Gebruiker (string gebruikersnaam_, string password_, string voornaam_, string achternaam_, int leeftijd)
+    {
+        gebruikersnaam_ = gebruikersnaam;
+        password_ = password;
+        voornaam_ = voornaam;
+        achternaam_ = achternaam;
+        leeftijd_ = leeftijd;
+    }
+}
